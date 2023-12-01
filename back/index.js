cc3cd421ea0708f9d087429878a2f2f711af2d80
zeluilo <princezel18@gmail.com>
2023-12-01 07:44:39 +0000

    workouts

--- back/index.js
+++ back/index.js
@@ -1,26 +1,29 @@
 require('dotenv').config()
+
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const UserModel = require('./models/Users')
+const workoutRoutes = require('./routes/workouts')
+// const mongoose = require('mongoose');
 
 // express app
 const app = express();
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+// app.use(cors());
+
 // Connect to MongoDB
-mongoose.connect("mongodb+srv://admin:admin123@hms.ofgfn7a.mongodb.net/test");
+// mongoose.connect("mongodb+srv://admin:admin123@hms.ofgfn7a.mongodb.net/test");
 
 // Define a route
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome bro'})
-});
+app.use('/thomas/workouts', workoutRoutes)
 
 // Start the server
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running`);
+  console.log(`Server is running`, process.env.PORT);
 });
 
